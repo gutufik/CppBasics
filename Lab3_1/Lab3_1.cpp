0a45064006d042480ff5fe40c986277a3fdea1ee
Михаил Мясников <gutufik@mail.ru>
2024-03-27 13:08:03 +0300

    add lab 3

---
+++ Lab3_1/Lab3_1.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <Windows.h>
+
+
+using namespace std;
+
+bool TryParseFloat(string str, float& result)
+{
+    if (sscanf_s(str.data(), "%f", &result) != 1) {
+        cout << "Невозможно преобразовать значение в число\n";
+        return false;
+    }
+    return true;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+    vector<float> arr;
+    float sum = 0;
+
+    string line;
+    int lineNumber = 1;
+
+    fstream file("Data.txt");
+
+    if (file.fail())
+    {
+        cout << "Не удается открыть файл";
+        return 1;
+    }
+
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+            float fline;
+
+            if (!TryParseFloat(line, fline)) {
+                cout << "Ошибка в строке " + to_string(lineNumber);
+                return 1;
+            }
+
+            if (fline > 0)
+            {
+                sum += stof(line);
+                arr.push_back(fline); // Добавлям в исходный вектор только положительные элементы
+            }
+            lineNumber++;
+        }
+    }
+    file.close();
+
+    if (arr.size() == 0) {
+        cout << "В файле нет положительных чисел";
+        return 1;
+    }
+
+    float avg = sum / arr.size();
+
+    vector<float> output;
+
+    for (float elem : arr)
+    {
+        if (elem > avg) output.push_back(elem);
+    }
+
+    cout << "Среднее значение положительных чисел = " << avg << endl;
+
+    if (output.size() == 0) {
+        cout << "В файле нет чисел превышающих среднее значение\n";
+        return 1;
+    }
+
+    cout << "Числа превышающие среднее значение:\n";
+    for (float elem : output)
+    {
+        cout << elem << endl;
+    }
+}
+
---
+++ Lab3_2/Lab3_2.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <array>
+#include <Windows.h>
+
+using namespace std;
+
+
+// Добавить проверки
+
+vector<string> SplitString(string s, string del) {
+    vector<string> words;
+    int end = s.find(del);
+    while (end != -1) {
+        words.push_back(s.substr(0, end));
+        s.erase(s.begin(), s.begin() + end + del.length());
+        end = s.find(del);
+    }
+    words.push_back(s.substr(0, end));
+
+    return words;
+}
+
+struct Bank
+{
+    string Title;
+    int* Loans;
+    int LoansCount;
+
+    Bank(string stringBank)
+    {
+        vector<string> cols = SplitString(stringBank, "\t");
+
+        int colsCount = cols.size();
+        LoansCount = colsCount - 1;
+
+        for (int i = 0; i < colsCount; i++)
+        {
+            if (i == 0)
+            {
+                Title = cols[i];
+                Loans = new int[colsCount - 1];
+            }
+            else
+            {
+                Loans[i - 1] = stoi(cols[i]); //stoi преобразование строки в int
+            }
+        }
+    }
+
+    int GetLoansSum()
+    {
+        int sum = 0;
+        if (sizeof(Loans) > 0)
+        {
+            for (int i = 0; i < LoansCount; i++)
+            {
+                sum += Loans[i];
+            }
+        }
+        return sum;
+    }
+
+    string ToString() 
+    {
+        string result = Title;
+
+        for (int i = 0; i < LoansCount; i++)
+        {
+            int li = Loans[i];
+            result += "\t" + to_string(li);
+        }
+        result += "\t" + to_string(GetLoansSum());
+
+        result += "\n";
+
+        return result;
+    }
+};
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+    string line;
+
+    fstream file("Input.txt");
+
+    fstream output("Output.txt");
+
+    vector<Bank> banks;
+
+    string header;
+
+    bool headerPassed = false;
+
+    if (file.fail())
+    {
+        cout << "Не удается открыть файл Input.txt";
+        return 1;
+    }
+    if (output.fail())
+    {
+        cout << "Не удается открыть файл Output.txt";
+        return 1;
+    }
+
+    if (file.is_open()) // Чтение файла
+    {
+        while (getline(file, line)) 
+        {
+            if (header == "")
+            {
+                header = line + "\t" + (string)"Общая сумма ссуд" + "\n";
+            }
+            else
+            {
+                banks.push_back(Bank(line));
+            }
+        }
+    }
+    file.close();
+
+    //Сортировка
+
+    for (int i = 0; i < banks.size(); i++)
+    {
+        for (int j = 0; j < banks.size() - i - 1; j++) 
+        {
+            if (banks[j].Title > banks[j + 1].Title)
+            {
+                swap(banks[j], banks[j + 1]);
+            }
+        }
+    }
+
+    //Запись в файл
+
+    output << header;
+
+    for (Bank bank : banks)
+    {
+        output << bank.ToString();
+    }
+
+    output.close();
+
+    cout << "Данные успешно отформатированы и перенесены файл Output.txt" << endl;
+}
+
