060df98a253c86d8777d2241a74b342880e9cf66
Михаил Мясников <gutufik@mail.ru>
2024-04-07 21:04:41 +0300

    add json file reading

--- Lab4_2/Lab4_2.cpp
+++ Lab4_2/Lab4_2.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <stdexcept>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+#include <json/json.h>
+#include <json/value.h>
+
+using namespace std;
 
 struct TabletScanner {
-    char* ModelName;
+    string ModelName;
     float Price;
     float HorizontalSize;
     float VerticalSize;
+
+    TabletScanner(string modelName, float price, float horizontalSize, float verticalsize)
+    {
+        ModelName = modelName;
+        Price = price;
+        HorizontalSize = horizontalSize;
+        VerticalSize = verticalsize;
+    }
 };
 
 
@@ -18,15 +34,49 @@ struct Node {
     }
 };
 
-
-
 struct LinkedList
 {
     Node* Head;
     int Length;
 
-    void PushBack(TabletScanner scanner) {
 
+    LinkedList()
+    {
+        Head = NULL;
+        Length = 0;
+    }
+
+
+    void PushBack(TabletScanner* scanner) {
+        Node* newNode = new Node(scanner);
+
+        if (Length == 0) {
+            Head = newNode;
+        }
+        else {
+            Node* lastNode = Head;
+            while (lastNode->Next != NULL)
+            {
+                lastNode = lastNode->Next;
+            }
+
+            lastNode->Next = newNode;
+        }
+        Length++;
+    }
+
+    TabletScanner operator[](int index) {
+        if (index < 0 || index >= Length)
+        {
+            throw out_of_range("Index out of range");
+        }
+
+        Node* resultNode = Head;
+
+        for (int i = 0; i < index; i++) {
+            resultNode = resultNode->Next;
+        }
+        return *resultNode->Value;
     }
 
     LinkedList Filter() {
@@ -37,5 +87,32 @@ struct LinkedList
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+    LinkedList* scannerList = new LinkedList();
+
+    fstream file("Data.json");
+    Json::Value json;
+    Json::Reader reader;
+
+    reader.parse(file, json);
+
+
+    for (int i = 0; i < json.size(); i++) {
+
+        string str = json[i]["ModelName"].asString().c_str();
+        TabletScanner* scanner = new TabletScanner
+        (
+            json[i]["ModelName"].asString().c_str(),
+            json[i]["Price"].asFloat(),
+            json[i]["HorizontalSize"].asFloat(),
+            json[i]["VerticalSize"].asFloat()
+        );
+
+        scannerList->PushBack(scanner);
+    }
+    cout << json << endl;
+
 }
